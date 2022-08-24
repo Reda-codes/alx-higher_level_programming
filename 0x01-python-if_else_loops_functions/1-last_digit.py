f9aa9af5ab68fce190e5616d8cfcafbcad9ddcc7
Reda <muhammadredabh@gmail.com>
2022-08-24 10:22:01 +0100

    fixing the failed 0 tests and pystyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 num = str(number)
+str = "and is less than 6 and not 0"
 if number > 0:
     if int(num[-1:]) == 0:
         print(f"Last digit of {number} is {num[-1:]} and is 0")
     elif int(num[-1:]) <= 5:
-        print(f"Last digit of {number} is {num[-1:]} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {num[-1:]} {str}")
     else:
         print(f"Last digit of {number} is {num[-1:]} and is greater than 5")
 elif number < 0:
     if int(num[-1:]) == 0:
         print(f"Last digit of {number} is {num[-1:]} and is 0")
     else:
-        print(f"Last digit of {number} is -{num[-1:]} and is less than 6 and not 0")
+        print(f"Last digit of {number} is -{num[-1:]} {str}")
+else:
+    print(f"Last digit of {number} is {num[-1:]} and is 0")
