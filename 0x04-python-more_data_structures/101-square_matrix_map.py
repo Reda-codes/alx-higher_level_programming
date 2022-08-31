d0208bfbeeefca93cf688cf766082fa27670571f
Reda <muhammadredabh@gmail.com>
2022-08-31 14:47:41 +0100

    fixed styling

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda arr: list(map(lambda i: i**2, arr)) ,matrix))
+    return list(map(lambda arr: list(map(lambda i: i**2, arr)), matrix))
