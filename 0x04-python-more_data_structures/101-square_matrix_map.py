0b7b2e6990a0aa4428c42831cd57115032531a62
Reda <muhammadredabh@gmail.com>
2022-08-31 14:46:34 +0100

    14. Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda matrix : [pow(i, 2) for i in matrix], matrix))
+    return list(map(lambda arr: list(map(lambda i: i**2, arr)) ,matrix))
