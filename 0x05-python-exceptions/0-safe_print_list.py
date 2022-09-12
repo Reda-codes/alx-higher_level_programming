d011124c565d8ba8a0b6ef9305e80ed9102b0020
Reda <muhammadredabh@gmail.com>
2022-09-12 10:10:32 +0100

    fixed exeprion handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             count += 1
             print(my_list[i], end='')
-        except:
+        except Exception as error:
             count -= 1
             break
     print('')
