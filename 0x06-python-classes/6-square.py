96363b7e94e52249de383787eb7b2e22428e034c
Reda <muhammadredabh@gmail.com>
2022-09-13 19:51:08 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,11 +43,11 @@ class Square:
         if (self.__size == 0):
             print("")
         else:
-            for i in range(self.position[1]):
+            for i in range(self.__position[1]):
                 print("")
 
-            for i in range(self.size):
-                print(" " * self.position[0] + "#" * self.size)
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
 
     @property
     def position(self):
