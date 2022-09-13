a431b69205710ece03a2eef11fc322206b1d1abe
Reda <muhammadredabh@gmail.com>
2022-09-13 10:29:29 +0100

    2ed challenge styling fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square
+"""
+
+
 class Square:
     """
     a class Square that defines a square
     """
     def __init__(self, size=0):
         """Inits Square with a private instance attribute: size."""
-        self.__size=size
+        self.__size = size
         if not type(size) is int:
             print("size must be an integer", end='')
             raise TypeError
