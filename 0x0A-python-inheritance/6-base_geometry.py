a1913f75dd3b3b4cf338fca0fdd4df18e23682e6
Reda <muhammadredabh@gmail.com>
2022-09-26 17:19:05 +0100

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that implements an object skeleton for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Body code
+    """
+    def area(self):
+        """area method
+
+        Raise:
+            Exception: area not implemented
+        """
+        raise Exception("area() is not implemented")
