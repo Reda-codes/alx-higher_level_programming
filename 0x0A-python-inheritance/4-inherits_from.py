0d2736b63f1b7edd620ad5d91839ccab902fe2f5
Reda <muhammadredabh@gmail.com>
2022-09-26 17:13:33 +0100

    fx

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
         boolean
     """
 
-    return type(obj) != a_class and issubclass(type(obj), a_class)
+    return (type(obj) != a_class and issubclass(type(obj), a_class))
