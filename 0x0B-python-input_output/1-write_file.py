cdb5f09a62c6f2da4582d19d2ea1bae9b3700282
Reda <muhammadredabh@gmail.com>
2022-09-27 14:44:31 +0100

    0. Read file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+
+def write_file(filename="", text=""):
+    """
+        function that writes a string to a text file
+        Args:
+            fileName: name of the file
+            text: string input
+    """
+    num = 0
+    with open(filename, mode="w", encoding="UTF-8") as file:
+        var = file.write(text)
+    
+    file.close()
+    return(var)
