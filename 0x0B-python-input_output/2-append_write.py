12f563d245da9f3a677709012b0db0695da66aa8
Reda <muhammadredabh@gmail.com>
2022-09-27 16:37:16 +0100

    2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+
+def append_write(filename="", text=""):
+    """
+        function that writes a string to a text file
+        Args:
+            fileName: name of the file
+            text: string input
+    """
+    with open(filename, mode="a", encoding="UTF-8") as file:
+        var = file.write(text)
+
+    file.close()
+    return(var)
