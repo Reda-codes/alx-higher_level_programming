f7f2220a210815008b8bb170f76148700f489854
Reda <muhammadredabh@gmail.com>
2022-11-28 15:15:05 +0100

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2])) {
+  for (let i = 1; i <= process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+} else {
+  console.log('Missing size');
+}
