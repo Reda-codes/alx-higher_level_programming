194b536cfcc074e7b1184a3e93ef26d107a29ae8
Reda <muhammadredabh@gmail.com>
2022-11-28 10:38:20 +0100

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] && process.argv[3]) {
   console.log('Arguments found');
-} else if (process.argv[2]) { 
+} else if (process.argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
