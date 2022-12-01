967dc4b94759c3b46ee7beff2b0d832f135261ba
Reda <muhammadredabh@gmail.com>
2022-12-01 16:53:05 +0100

    style fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+script that takes in arguments and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+that is safe from MySQL injections!
+"""
+import sys
+import MySQLdb
+
+
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = db.cursor()
