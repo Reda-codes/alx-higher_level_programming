b4b7aeca4bd52a41390483dbc1dc114843febc71
Reda <muhammadredabh@gmail.com>
2022-12-01 16:59:44 +0100

    styling fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+ script that lists all cities from the database hbtn_0e_4_usa
+"""
+import sys
+import MySQLdb
+
+
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = db.cursor()
