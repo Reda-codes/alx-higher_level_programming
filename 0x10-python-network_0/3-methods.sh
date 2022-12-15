bd690f88d4d6c6ea470e4d0eaa1d9e08cdb1a6ec
Reda <muhammadredabh@gmail.com>
2022-12-15 16:11:18 +0100

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes a URL and displays all HTTP methods the server will accept.
-curl -sIL -X get "$1" | grep Allow | cut -d " " -f2
+curl -sIL -X OPTIONS "$1" | grep Allow: | cut -d ":" -f2 | sed 's/ //'
