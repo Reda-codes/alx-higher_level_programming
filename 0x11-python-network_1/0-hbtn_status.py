ed5b48b6ce59ee9def89cebbbb95e04b1f3a5b56
Reda <muhammadredabh@gmail.com>
2022-12-16 11:50:44 +0100

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    data = response.read()
+    print('Body response:')
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('UTF-8')}")
