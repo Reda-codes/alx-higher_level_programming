e4baf4bc02d66c7371f976ae84f566b790872316
Reda <muhammadredabh@gmail.com>
2022-12-16 16:51:30 +0100

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that sends a request to a URL and displays the response body"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print(f"Error code: {req.status_code}")
+    else:
+        print(f"{r.text}")
