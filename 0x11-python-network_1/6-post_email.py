d37df0421a48930488ed760dc8cbe8fcc0432771
Reda <muhammadredabh@gmail.com>
2022-12-16 16:37:22 +0100

    6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Script sends a POST request to the passed URL with the email parameter"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1]), {'email': argv[2]}
+    print(r.text)
